ac6114f22de7326932c0bce3e18895b4b2bf1600
Adarsh-sri-1805 <adarshsri107@gmail.com>
2025-08-21 09:51:31 +0530

    done

---
+++ src/mymain1.java
@@ -0,0 +1,7 @@
+public class mymain1 {
+    public static void main(String[] args) {
+        CollegeStudent st = new CollegeStudent("Delhi",13);
+        st.show_roll();
+        st.show_address();
+    }
+}
